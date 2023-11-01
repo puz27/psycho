11120a347afee98b50763e2ac1dc5648ac691e3f
anv <anv@woori.ru>
2023-11-01 17:06:32 +0300

    add main.html

--- portal/views.py
+++ portal/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from portal.models import Work
 
 
 class MainView(ListView):
+    model = Work
     template_name = "mailing/main.html"
 
     def get_context_data(self, *, object_list=None, **kwargs):
+
         context = super().get_context_data(**kwargs)
         return context
 
