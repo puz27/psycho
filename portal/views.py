eb40a9c508dcda85a7166ae2156e0111106d6aea
anv <anv@woori.ru>
2023-11-01 18:59:42 +0300

    add tempaltes

--- portal/views.py
+++ portal/views.py
@@ -5,10 +5,11 @@ from portal.models import Work
 
 class MainView(ListView):
     model = Work
-    template_name = "mailing/main.html"
+    template_name = "portal/main.html"
+    extra_context = {'title': 'Главная страница'}
+
 
     def get_context_data(self, *, object_list=None, **kwargs):
-
         context = super().get_context_data(**kwargs)
         return context
 
